4d6e79682154df423e51ac49ba35b583edfe1716
Yeuoly <admin@srmxy.cn>
2023-08-18 19:12:26 +0800

    add example and kasumi python sdk

---
+++ python/kasumi/embedding.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+# This file contains the functions used to insert and retrieve embeddings from the Kasumi database.
+from typing import List
+
+def insert_embedding(embedding: List[float]) -> bool:
+    """
+    This function is used to insert an embedding into the Kasumi database.
+
+    :param embedding: The embedding to insert.
+    :return: True if the embedding was inserted successfully, False otherwise.
+    """
+    pass
