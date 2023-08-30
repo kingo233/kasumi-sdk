b05b94c988a094c95106c566823b84191d0544c6
jiangnan <kingo233@outlook.com>
2023-08-30 10:34:46 +0800

    bugfix:setup

--- python/setup.py
+++ python/setup.py
@@ -18,11 +18,11 @@ URL = 'https://github.com/Meduoduo/kasumi-sdk'
 EMAIL = 'kasumi@miduoduo.org'
 AUTHOR = 'MIDUODUO LTD.'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'requests', 'flask', 'json',
+    'requests', 'flask', 'eventlet',
 ]
 
 # What packages are optional?
